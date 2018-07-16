dfb9ce9115d1ece0522b3deebcb2236fd63e4b65
Tuyen Le Chau <Tuyen.LeChau@nashtechglobal.com>
2018-07-16 16:15:51 +0700

    add question related entity with in line form

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {
+            'classes': ['collapse'],
+            'fields': ['pub_date']
+        })
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
