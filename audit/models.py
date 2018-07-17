2f7005de1ddbb6a08a41606def73d05a681bbd95
Tuyen Le Chau <Tuyen.LeChau@nashtechglobal.com>
2018-07-17 15:41:06 +0700

    add model User

--- audit/models.py
+++ audit/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=100)
+    role = models.CharField(max_length=30)
+    is_active = models.BooleanField
+
+    def __str__(self):
+        return self.username
