10a2d058773a1e0115ae4de23648fcf17983254f
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-10 23:17:52 +0700

    update blog_posts model

--- blog_posts/models.py
+++ blog_posts/models.py
@@ -7,9 +7,11 @@ from django.db import models
 
 
 class blog_posts(models.Model):
-    title = models.CharField(max_length=400)
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True, null=True)
     tag = models.CharField(max_length=50)
     author = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
 
     def __unicode__(self):
         return self.title
