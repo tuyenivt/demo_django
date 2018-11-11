211c900fc3ea2ebd75ca72b04ed9534979bb48a6
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-11 22:44:56 +0700

    using serializer and test post method

--- django-rest-framework/profiles_project/profiles_api/views.py
+++ django-rest-framework/profiles_project/profiles_api/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
   """Test API VIew."""
+
+  serializer_class = serializers.HelloSerializer
 
   def get(self, request, format=None):
     """Returns a list of APIView features."""
@@ -16,3 +20,14 @@ class HelloApiView(APIView):
       'Is mapped manually to URLs'
     ]
     return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+  def post(self, request):
+    """Create a hello message with our name"""
+    serializer = serializers.HelloSerializer(data=request.data)
+    if serializer.is_valid():
+      name = serializer.data.get('name')
+      message = 'Hello {0}'.format(name)
+      return Response({'message': message})
+    else:
+      return Response(
+        serializer.errors, status=status.HTTP_400_BAD_REQUEST)
