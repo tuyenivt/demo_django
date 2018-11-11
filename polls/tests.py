ceb605f13174a4b78d71a8067a4f1bc778b9c589
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-11 13:24:04 +0700

    trailing whitespace

--- polls/tests.py
+++ polls/tests.py
@@ -10,7 +10,7 @@ from polls.models import Question
 
 class QuestionModelTests(TestCase):
     def test_was_published_recently_with_old_question(self):
-        """ 
+        """
         was_published_recently() returns False for questions whose pub_date is older than 1 day
         """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
@@ -18,7 +18,7 @@ class QuestionModelTests(TestCase):
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
-        """ 
+        """
         was_published_recently() returns True for questions whose pub_date is within the last date
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
@@ -26,7 +26,7 @@ class QuestionModelTests(TestCase):
         self.assertIs(recent_question.was_published_recently(), True)
 
     def test_was_published_recently_with_future_question(self):
-        """ 
+        """
         was_published_recently() returns False for questions whose pub_date is in the future
         """
         time = timezone.now() + datetime.timedelta(days=30)
