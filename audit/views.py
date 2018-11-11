ca168937dac660c6cefdbfbfeebe617eac81d799
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-11 13:26:04 +0700

    remove unused import

--- audit/views.py
+++ audit/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.views import generic
-from django.utils import timezone
 
 # Create your views here.
 
