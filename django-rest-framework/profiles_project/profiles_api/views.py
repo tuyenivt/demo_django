6f63e4382f24a72c64a5ac418aa72386326e8d18
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-12 21:37:21 +0700

    add a sample viewset

--- django-rest-framework/profiles_project/profiles_api/views.py
+++ django-rest-framework/profiles_project/profiles_api/views.py
@@ -1,3 +1,4 @@
+from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -42,3 +43,15 @@ class HelloApiView(APIView):
   def delete(self, request, pk=None):
     """Delete an object"""
     return Response({'method': 'delete'})
+
+class HelloViewSet(viewsets.ViewSet):
+  """Test API ViewSet"""
+
+  def list(self, request):
+    """Return a hello message"""
+    a_viewset = [
+      'Uses actions (list, create, retrieve, update, partial_update)',
+      'Automatically maps to URLs using Routers',
+      'Provides more functionality with less code'
+    ]
+    return Response({'message': 'Hello!', 'a_viewset': a_viewset})
