1f655c249675a34105d8af625d6fa895c6d08d4d
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-12 20:46:18 +0700

    add put, patch, delete method

--- django-rest-framework/profiles_project/profiles_api/views.py
+++ django-rest-framework/profiles_project/profiles_api/views.py
@@ -30,3 +30,15 @@ class HelloApiView(APIView):
     else:
       return Response(
         serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def put(self, request, pk=None):
+    """Handles updating an object"""
+    return Response({'method': 'put'})
+
+  def patch(self, request, pk=None):
+    """Patch request, only update fields provided in the request"""
+    return Response({'method': 'patch'})
+
+  def delete(self, request, pk=None):
+    """Delete an object"""
+    return Response({'method': 'delete'})
