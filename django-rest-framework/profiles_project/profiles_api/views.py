5e43f9bfa820548b8c43595bcfff437e9fa96d7c
Tuyen Le Chau <tuyenivt@gmail.com>
2018-11-13 21:05:40 +0700

    create login api viewset

--- django-rest-framework/profiles_project/profiles_api/views.py
+++ django-rest-framework/profiles_project/profiles_api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+
 from . import serializers, models, permissions
 
 # Create your views here.
@@ -94,3 +97,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   permission_classes = (permissions.UpdateOwnProfile,)
   filter_backends = (filters.SearchFilter,)
   search_fields = ('name', 'email')
+
+class LoginViewSet(viewsets.ViewSet):
+  """Check email and password and return an auth token"""
+  serializer_class = AuthTokenSerializer
+  def create(self, request):
+    """Use the ObtainAuthToken APIView to validate and create a token"""
+    return ObtainAuthToken().post(request)
