1a86ff45de41d9868cadfab1787f1f5431a1e581
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-24 21:57:45 +0700

    tdd - add list view test

--- django/tdd/blog/tests.py
+++ django/tdd/blog/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from .models import Entry
 
 class EntryModelTest(TestCase):
@@ -15,3 +16,24 @@ class ProjectTest(TestCase):
   def test_homepage(self):
     response = self.client.get('/')
     self.assertEqual(response.status_code, 200)
+
+class HomePageTests(TestCase):
+
+  """ Test whether our blog entries show up on the homepage """
+
+  def setUp(self):
+    self.user = get_user_model().objects.create(username='some_user')
+
+  def test_one_entry(self):
+    Entry.objects.create(title='1-title', body='1-body', author=self.user)
+    response = self.client.get('/')
+    self.assertContains(response, '1-title')
+    self.assertContains(response, '1-body')
+
+  def test_two_entry(self):
+    Entry.objects.create(title='1-title', body='1-body', author=self.user)
+    Entry.objects.create(title='2-title', body='2-body', author=self.user)
+    response = self.client.get('/')
+    self.assertContains(response, '1-title')
+    self.assertContains(response, '1-body')
+    self.assertContains(response, '2-title')
