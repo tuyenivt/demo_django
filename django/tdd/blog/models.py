17904761e519967087845278105c2d9a2316736f
Tuyen Le Chau <tuyen.lechau@nashtechglobal.com>
2018-12-24 18:20:13 +0700

    tdd - refactor test

--- django/tdd/blog/models.py
+++ django/tdd/blog/models.py
@@ -6,3 +6,6 @@ class Entry(models.Model):
   body = models.TextField()
   created_at = models.DateTimeField(auto_now_add = True, editable = False)
   modified_at = models.DateTimeField(auto_now =True, editable = False)
+
+  def __str__(self):
+    return self.title
