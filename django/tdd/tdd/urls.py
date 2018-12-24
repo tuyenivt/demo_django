f7271ac1d123f0992b2f50a1ca5c539ee155fc1a
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-24 19:21:37 +0700

    tdd - refactor - add home page

--- django/tdd/tdd/urls.py
+++ django/tdd/tdd/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
 ]
