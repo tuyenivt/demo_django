26b04e5e6d5dc41a54f6e28c472885f34ad7ba5c
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-24 22:13:38 +0700

    tdd - refactor and pass test

--- django/tdd/blog/views.py
+++ django/tdd/blog/views.py
@@ -1,4 +1,6 @@
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import Entry
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'index.html'
+    queryset = Entry.objects.order_by('-created_at')
