77dc87ef2e1de96d9ffe92edd90b6f381a44bb61
Tuyen Le Chau <tuyen.lechau@nashtechglobal.com>
2018-12-24 18:25:08 +0700

    tdd - add new test - correct the pluralization of entry is misspelled (Entrys -> Entries)

--- django/tdd/blog/tests.py
+++ django/tdd/blog/tests.py
@@ -6,3 +6,6 @@ class EntryModelTest(TestCase):
   def test_string_representation(self):
     entry = Entry(title='My entry title')
     self.assertEqual(str(entry), entry.title)
+
+  def test_verbose_name_plural(self):
+    self.assertEqual(str(Entry._meta.verbose_name_plural), "entries")
