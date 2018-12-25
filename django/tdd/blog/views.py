8037910e6092588995efbb38f52cbda53ff8fa8a
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-25 18:33:32 +0700

    tdd - refactor and pass test

--- django/tdd/blog/views.py
+++ django/tdd/blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from .forms import CommentForm
 from .models import Entry
 
 class HomeView(ListView):
@@ -7,3 +8,5 @@ class HomeView(ListView):
 
 class EntryDetail(DetailView):
     model = Entry
+    template_name = 'blog/entry_detail.html'
+    form_class = CommentForm
