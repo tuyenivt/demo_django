faaeafac400e43f8ef5c60182f7e592c5b1a59b4
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-25 18:20:42 +0700

    tdd - refactor add save method and pass test

--- django/tdd/blog/forms.py
+++ django/tdd/blog/forms.py
@@ -11,3 +11,9 @@ class CommentForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.entry = kwargs.pop('entry') # the blog entry instance
         super().__init__(*args, **kwargs)
+
+    def save(self):
+        comment = super().save(commit=False)
+        comment.entry = self.entry
+        comment.save()
+        return comment
