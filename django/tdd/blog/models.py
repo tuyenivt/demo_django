790a4b63f8d0c4ad8ad28e29cb20f97154a2a1ec
Tuyen Le Chau <tuyenivt@gmail.com>
2018-12-25 10:31:02 +0700

    tdd - refactor and pass test

--- django/tdd/blog/models.py
+++ django/tdd/blog/models.py
@@ -24,3 +24,6 @@ class Comment(models.Model):
   body = models.TextField()
   created_at = models.DateTimeField(auto_now_add=True, editable=False)
   modified_at = models.DateTimeField(auto_now=True, editable=False)
+
+  def __str__(self):
+    return self.body
